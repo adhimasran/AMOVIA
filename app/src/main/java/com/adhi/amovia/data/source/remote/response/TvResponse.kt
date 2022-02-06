9c3c1620846052e8430ad539ee07d658e8de0b2d
adhimasran <adhicittamasran@gmail.com>
2022-02-06 20:59:57 +0800

    Create tv data response

---
+++ app/src/main/java/com/adhi/amovia/data/source/remote/response/TvResponse.kt
@@ -0,0 +1,20 @@
+package com.adhi.amovia.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class TvResponse(
+    @field:SerializedName("id")
+    val id: Int,
+
+    @field:SerializedName("poster_path")
+    val posterPath: String,
+
+    @field:SerializedName("backdrop_path")
+    val backdropPath: String,
+
+    @field:SerializedName("name")
+    val name: String,
+
+    @field:SerializedName("vote_average")
+    val rating: Double
+)
