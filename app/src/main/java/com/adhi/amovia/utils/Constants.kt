0ba251234c8fe76ed5de3ef0638a08b3ff2a15ec
adhimasran <adhicittamasran@gmail.com>
2022-02-07 17:00:40 +0800

    Adding constants

--- app/src/main/java/com/adhi/amovia/utils/Constants.kt
+++ app/src/main/java/com/adhi/amovia/utils/Constants.kt
@@ -1,4 +1,9 @@
 package com.adhi.amovia.utils
 
+import com.adhi.amovia.BuildConfig
+
 object Constants {
+    const val API_KEY = BuildConfig.KEY
+    const val BASE_URL = BuildConfig.BaseUrl
+    const val BASE_URL_IMG = BuildConfig.BaseImageUrl
 }
