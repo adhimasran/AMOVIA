6c53b95ab59bf483e9034809914f7cd95c6f4041
adhimasran <adhicittamasran@gmail.com>
2022-02-08 20:15:14 +0800

    Create setupAdapter function

--- app/src/main/java/com/adhi/amovia/ui/home/HomeFragment.kt
+++ app/src/main/java/com/adhi/amovia/ui/home/HomeFragment.kt
@@ -10,15 +10,14 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.adhi.amovia.R
 import com.adhi.amovia.databinding.FragmentHomeBinding
+import com.adhi.amovia.ui.home.adapter.UpcomingMovieAdapter
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private val binding get() = _binding
+    private val upcomingMovieAdapter = UpcomingMovieAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,13 +28,19 @@ class HomeFragment : Fragment() {
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding?.root
+    }
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupAdapter()
+    }
+
+    private fun setupAdapter() {
+        binding?.apply {
+            rvUpcomingMovie.adapter = upcomingMovieAdapter
+        }
     }
 
     override fun onDestroyView() {
