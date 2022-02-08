e8bd7b21a4eb47e24a758f8b513ba24063148b9d
adhimasran <adhicittamasran@gmail.com>
2022-02-08 19:52:32 +0800

    Create ViewModel function to get upcoming movies

--- app/src/main/java/com/adhi/amovia/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/adhi/amovia/ui/home/HomeViewModel.kt
@@ -3,11 +3,31 @@ package com.adhi.amovia.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.adhi.amovia.data.api.ApiConfig
+import com.adhi.amovia.data.source.remote.response.FilmResponse
+import com.adhi.amovia.data.source.remote.response.ListFilm
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class HomeViewModel : ViewModel() {
+    private val apiConfig = ApiConfig.getApiService()
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    fun getUpcomingMovie(): LiveData<List<FilmResponse>> {
+        val listMovie = MutableLiveData<List<FilmResponse>>()
+        val client = apiConfig.getFilm("movie", "upcoming")
+
+        client.enqueue(object : Callback<ListFilm> {
+            override fun onResponse(call: Call<ListFilm>, response: Response<ListFilm>) {
+                listMovie.value = response.body()?.results
+            }
+
+            override fun onFailure(call: Call<ListFilm>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        return listMovie
     }
-    val text: LiveData<String> = _text
 }
