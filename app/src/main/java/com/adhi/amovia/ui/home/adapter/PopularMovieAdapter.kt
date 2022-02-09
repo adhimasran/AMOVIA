b8943b830d0041958e3cbe65d34d6d510cc293b7
adhimasran <adhicittamasran@gmail.com>
2022-02-09 14:43:11 +0800

    Create popular movie adapter

---
+++ app/src/main/java/com/adhi/amovia/ui/home/adapter/PopularMovieAdapter.kt
@@ -0,0 +1,45 @@
+package com.adhi.amovia.ui.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.adhi.amovia.data.source.remote.response.FilmResponse
+import com.adhi.amovia.databinding.ItemsListHomeBinding
+import com.adhi.amovia.utils.Utility.loadImage
+
+class PopularMovieAdapter : RecyclerView.Adapter<PopularMovieAdapter.MovieViewHolder>() {
+    private val listMovie = ArrayList<FilmResponse>()
+
+    fun setMovie(movie: List<FilmResponse>) {
+        listMovie.clear()
+        listMovie.addAll(movie)
+        notifyDataSetChanged()
+    }
+
+    inner class MovieViewHolder(private val binding: ItemsListHomeBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(popularMovie: FilmResponse) {
+            binding.apply {
+                popularMovie.apply {
+                    tvTitle.text = title
+                    tvRating.text = rating.toString()
+                    imgPoster.loadImage(posterPath)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val binding =
+            ItemsListHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MovieViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = listMovie[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount(): Int = listMovie.size
+}
