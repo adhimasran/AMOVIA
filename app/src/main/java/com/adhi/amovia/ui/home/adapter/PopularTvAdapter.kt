272bd0eb8751e4b359cf5b95e3f285683c96612e
adhimasran <adhicittamasran@gmail.com>
2022-02-10 09:57:46 +0800

    Create popular TV adapter

---
+++ app/src/main/java/com/adhi/amovia/ui/home/adapter/PopularTvAdapter.kt
@@ -0,0 +1,42 @@
+package com.adhi.amovia.ui.home.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.adhi.amovia.data.source.remote.response.FilmResponse
+import com.adhi.amovia.databinding.ItemsListHomeLargeBinding
+import com.adhi.amovia.utils.Utility.loadImage
+
+class PopularTvAdapter : RecyclerView.Adapter<PopularTvAdapter.TvViewHolder>() {
+    private val listTv = ArrayList<FilmResponse>()
+
+    fun setTv(tv: List<FilmResponse>) {
+        listTv.clear()
+        listTv.addAll(tv)
+        notifyDataSetChanged()
+    }
+
+    inner class TvViewHolder(private val binding: ItemsListHomeLargeBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+                fun bind(popularTv: FilmResponse) {
+                    binding.apply {
+                        popularTv.apply {
+                            tvTitle.text = name
+                            imgPoster.loadImage(backdropPath)
+                        }
+                    }
+                }
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TvViewHolder {
+        val binding = ItemsListHomeLargeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TvViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: TvViewHolder, position: Int) {
+        val tv = listTv[position]
+        holder.bind(tv)
+    }
+
+    override fun getItemCount(): Int = listTv.size
+}
