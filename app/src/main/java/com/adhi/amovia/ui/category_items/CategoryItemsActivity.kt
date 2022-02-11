4665bda3306668ca5ad3fbd8a7436802eb640629
adhimasran <adhicittamasran@gmail.com>
2022-02-11 14:41:46 +0800

    Create CategoryItemsActivity class

---
+++ app/src/main/java/com/adhi/amovia/ui/category_items/CategoryItemsActivity.kt
@@ -0,0 +1,19 @@
+package com.adhi.amovia.ui.category_items
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.adhi.amovia.databinding.ActivityCategoryItemsBinding
+
+class CategoryItemsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCategoryItemsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCategoryItemsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    companion object {
+        const val EXTRA_FILM = "extra_film"
+    }
+}
