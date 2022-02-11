ee21e9ec6689422a52a0ad96662e2b6782833b19
adhimasran <adhicittamasran@gmail.com>
2022-02-11 15:42:35 +0800

    Setup the category items RecyclerView and adapter

--- app/src/main/java/com/adhi/amovia/ui/category_items/CategoryItemsActivity.kt
+++ app/src/main/java/com/adhi/amovia/ui/category_items/CategoryItemsActivity.kt
@@ -2,10 +2,12 @@ package com.adhi.amovia.ui.category_items
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import com.adhi.amovia.databinding.ActivityCategoryItemsBinding
 
 class CategoryItemsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCategoryItemsBinding
+    private val categoryItemsAdapter = CategoryItemsAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +16,15 @@ class CategoryItemsActivity : AppCompatActivity() {
 
         val mediaType = intent.getStringExtra(EXTRA_MEDIA)
         val category = intent.getStringExtra(EXTRA_CATEGORY)
+
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvFilm.apply {
+            adapter = categoryItemsAdapter
+            layoutManager = GridLayoutManager(this@CategoryItemsActivity, 2)
+        }
     }
 
     companion object {
