0c452ce6f71891de52066406590c60832dfc605a
adhimasran <adhicittamasran@gmail.com>
2022-02-12 22:57:47 +0800

    Create convertToDate function

--- app/src/main/java/com/adhi/amovia/utils/Utility.kt
+++ app/src/main/java/com/adhi/amovia/utils/Utility.kt
@@ -1,7 +1,11 @@
 package com.adhi.amovia.utils
 
+import android.os.Build
 import android.widget.ImageView
+import androidx.annotation.RequiresApi
 import com.bumptech.glide.Glide
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 object Utility {
     fun ImageView.loadImage(path: String) {
@@ -12,5 +16,11 @@ object Utility {
 
     fun Int.convertToTime(): String {
         return "${if (this / 60 > 0) "${this / 60}h " else ""}${this % 60}m"
+    }
+
+    fun String.convertToDate(): String {
+        val format = DateTimeFormatter.ofPattern("MMMM dd, yyyy")
+        val date = LocalDate.parse(this)
+        return date.format(format)
     }
 }
