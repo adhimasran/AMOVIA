4938254fae7438725b67980d10ef2b34ffb23389
adhimasran <adhicittamasran@gmail.com>
2022-02-13 17:18:52 +0800

    Create companion object to get data from intent

--- app/src/main/java/com/adhi/amovia/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/adhi/amovia/ui/detail/DetailActivity.kt
@@ -2,10 +2,16 @@ package com.adhi.amovia.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adhi.amovia.R
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+    }
+
+    companion object {
+        const val EXTRA_MEDIA = "extra_media"
+        const val EXTRA_ID = "extra_id"
     }
 }
